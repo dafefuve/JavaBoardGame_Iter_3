dc0722bf36cacb33ffd7000bd36260f5a8ac0c0f
Horacio <horacio.delgado23@gmail.com>
2014-04-15 18:49:16 -0400

    Updated Palace class

--- Iter3/src/Model/Palace.java
+++ Iter3/src/Model/Palace.java
@@ -4,4 +4,17 @@ package model;
  * Created by Daniel on 4/14/2014.
  */
 public class Palace {
+    private int palaceLevel;
+
+    public Palace(int palaceLevel){
+        this.palaceLevel = palaceLevel;
+    }
+
+    public int getPalaceLevel(){
+        return palaceLevel;
+    }
+
+    public void setPalaceLevel(int palaceLevel){
+        this.palaceLevel = palaceLevel;
+    }
 }
