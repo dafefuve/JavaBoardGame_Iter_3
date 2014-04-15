fc30eeb6b6f0e4da239a73681ab42b4e57946d50
Horacio <horacio.delgado23@gmail.com>
2014-04-15 17:43:37 -0400

    Updated Space

--- Iter3/src/Model/Space.java
+++ Iter3/src/Model/Space.java
@@ -1,7 +1,39 @@
 package model;
 
+import java.util.Stack;
+
 /**
  * Created by Daniel on 4/14/2014.
  */
 public class Space {
+    public Developer developer;
+    public Palace palace;
+    public Stack<TileComponent> land;
+
+    public Space(){
+        developer = null;
+        palace = null;
+        land = null;
+    }
+
+    public void addTileComponent(TileComponent tileComponent){
+        land.push(tileComponent);
+    }
+
+    public Integer getLevel(){
+        if(!land.empty())
+            return land.size();
+        return 0;
+    }
+
+    public void setDeveloper(Developer developer){
+        this.developer = developer;
+    }
+
+    public void setPalace(Palace palace){
+        this.palace = palace;
+    }
+
+
 }
+
