5b27c815298dc946c9853c1905270f24e9fc5da8
AlexBujduveanu <alexbujduveanu@gmail.com>
2014-04-15 16:40:00 -0400

    Added extra controllers
    Added shells for the extra controllers

---
+++ Iter3/src/controller/ActiveController.java
@@ -0,0 +1,61 @@
+package controller;
+
+/**
+ * Created by alexbujduveanu on 4/15/14.
+ */
+public class ActiveController
+{
+    private BoardController boardController;
+    private PlayerController playerController;
+    private ResourceController resourceController;
+
+    public ActiveController( BoardController b,  PlayerController p, ResourceController r )
+    {
+        boardController = b;
+        playerController = p;
+        resourceController = r;
+    }
+
+    public void moveN()
+    {
+        
+    }
+
+    public void moveNE()
+    {
+
+    }
+
+    public void moveNW()
+    {
+
+    }
+
+    public void moveS()
+    {
+
+    }
+
+    public void moveSE()
+    {
+
+    }
+
+    public void moveSW()
+    {
+
+    }
+
+    public void placeDeveloper()
+    {
+
+    }
+
+    public void moveDeveloper()
+    {
+
+    }
+
+
+
+}
