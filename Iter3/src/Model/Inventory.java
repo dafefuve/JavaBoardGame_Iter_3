bdf4ccffc6706c11bc3a6e3f83221858e0aef665
Horacio <horacio.delgado23@gmail.com>
2014-04-15 16:46:05 -0400

    Updated the inventory subsystem

--- Iter3/src/Model/Inventory.java
+++ Iter3/src/Model/Inventory.java
@@ -1,7 +1,12 @@
-package Model;
+package model;
 
 /**
  * Created by Daniel on 4/14/2014.
  */
-public class Inventory   {
+public abstract class Inventory   {
+
+    public abstract void setItem(String item, Integer count);
+    public abstract Integer getItem(String item);
+
+
 }
--- Iter3/src/Model/PlayerInventory.java
+++ Iter3/src/Model/PlayerInventory.java
@@ -1,7 +1,42 @@
-package Model;
+package model;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Daniel on 4/14/2014.
  */
-public class PlayerInventory {
+public class PlayerInventory extends Inventory{
+
+    private List<PalaceCard> hand;
+    private Map<String,Integer> items;
+
+    public PlayerInventory(List<PalaceCard> initHand, Map<String,Integer> initItems){
+        hand = initHand;
+        items = initItems;
+    }
+
+    public void setItem(String item, Integer count){
+        items.put(item,count);
+    }
+
+    public Integer getItem(String item){
+        return items.get(item);
+    }
+
+    public void addPalaceCard(PalaceCard card){
+        hand.add(card);
+    }
+
+    public PalaceCard removePalaceCard(PalaceCard card){
+        hand.remove(card);
+        return card;
+    }
+
+    public List<PalaceCard> getHand(){
+        return hand;
+    }
+
+
+
 }
