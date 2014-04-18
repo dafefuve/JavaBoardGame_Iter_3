050592095ae6065fda9bcbcae22c134e32a0b343
Arnav Agarwal <arnavagarwal@hotmail.com>
2014-04-17 20:49:12 -0400

    Some error removal

--- Iter3/src/model/LandType.java
+++ Iter3/src/model/LandType.java
@@ -3,12 +3,8 @@ package model;
 /**
  * Created by Horacio on 4/15/14.
  */
-<<<<<<< HEAD
 public abstract class LandType
 {
-=======
-public class LandType {
->>>>>>> aa8eb647a7517c455be13636bad76bc0d15c7081
     private String type;
 
     public LandType(String type)
