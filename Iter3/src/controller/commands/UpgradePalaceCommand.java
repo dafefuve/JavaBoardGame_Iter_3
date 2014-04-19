9e0ed8c590c894c949b0f8f9aefc13b4b335e9cb
William Frazee <frazeew@gmail.com>
2014-04-19 19:56:41 -0400

    Added UpgradePalaceCommand
     Also in the middle of modifying a looooooot of commands...

---
+++ Iter3/src/controller/commands/UpgradePalaceCommand.java
@@ -0,0 +1,18 @@
+package controller.commands;
+
+import controller.BoardController;
+import controller.Command;
+import controller.GameController;
+
+public class UpgradePalaceCommand(){
+	private int level;//the amount by which you wish to upgrade
+	public UpgradePalaceCommand(int level){
+
+	}
+	public void execute(){
+
+	}
+	public void undo(){
+		
+	}
+}
