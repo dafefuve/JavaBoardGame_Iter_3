b38c415d1008dfb51eff8bad2a6b21fcf1d119bc
Horacio <horacio.delgado23@gmail.com>
2014-04-19 18:56:56 -0400

    Updated MoveDeveloperAction

--- Iter3/src/controller/actions/MoveDeveloperAction.java
+++ Iter3/src/controller/actions/MoveDeveloperAction.java
@@ -1,7 +1,26 @@
 package controller.actions;
 
+import controller.Command;
+import controller.Facade;
+
+import java.awt.event.ActionEvent;
+
 /**
  * Created by Horacio on 4/18/14.
  */
 public class MoveDeveloperAction {
+    private Facade facade;
+
+    public MoveDeveloperAction(Facade facade)
+    {
+        this.facade = facade;
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        //facade.getViewController().placeOneBlock();
+        Command c = facade.getCommandController().moveDeveloperCommand(facade.getBoardController());
+        facade.getCommandStackController().push(c);
+        facade.getCommandController().execute(c);
+    }
 }
