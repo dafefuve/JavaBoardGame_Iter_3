d230f3bc7681d04b09e35ddbd3470e69dad796e3
AlexBujduveanu <alexbujduveanu@gmail.com>
2014-04-20 17:17:56 -0400

    More actions
    Two new actions

---
+++ Iter3/src/controller/actions/SaveGameAction.java
@@ -0,0 +1,14 @@
+package controller.actions;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * Created by Horacio on 4/18/14.
+ */
+public class SaveGameAction extends AbstractAction{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
