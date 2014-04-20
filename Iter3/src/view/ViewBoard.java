ddb6843c01d69d053e329d24e439dbb5dc99d795
Arnav Agarwal <arnavagarwal@hotmail.com>
2014-04-19 21:20:02 -0400

    Fixed the push() and peek() methods in View Board
    Changed all occurrences of peek() to peekIntoBoard() and all occurrences
    of push() to pushIntoBoard() in ViewBoard.java

--- Iter3/src/view/ViewBoard.java
+++ Iter3/src/view/ViewBoard.java
@@ -92,155 +92,155 @@ public class ViewBoard
         y = board[2][4].peekIntoStack().getCenterY();
         board[2][4].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][5].peek().getCenterX();
-        y = board[2][5].peek().getCenterY();
-        board[2][5].push(new ViewHexCentralJava(x, y));
+        x = board[2][5].peekIntoStack().getCenterX();
+        y = board[2][5].peekIntoStack().getCenterY();
+        board[2][5].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][7].peek().getCenterX();
-        y = board[2][7].peek().getCenterY();
-        board[2][7].push(new ViewHexCentralJava(x, y));
+        x = board[2][7].peekIntoStack().getCenterX();
+        y = board[2][7].peekIntoStack().getCenterY();
+        board[2][7].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][8].peek().getCenterX();
-        y = board[2][8].peek().getCenterY();
-        board[2][4].push(new ViewHexCentralJava(x, y));
+        x = board[2][8].peekIntoStack().getCenterX();
+        y = board[2][8].peekIntoStack().getCenterY();
+        board[2][4].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][10].peek().getCenterX();
-        y = board[2][10].peek().getCenterY();
-        board[2][10].push(new ViewHexCentralJava(x, y));
+        x = board[2][10].peekIntoStack().getCenterX();
+        y = board[2][10].peekIntoStack().getCenterY();
+        board[2][10].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][11].peek().getCenterX();
-        y = board[2][11].peek().getCenterY();
-        board[2][11].push(new ViewHexCentralJava(x, y));
+        x = board[2][11].peekIntoStack().getCenterX();
+        y = board[2][11].peekIntoStack().getCenterY();
+        board[2][11].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][13].peek().getCenterX();
-        y = board[2][13].peek().getCenterY();
-        board[2][13].push(new ViewHexCentralJava(x, y));
+        x = board[2][13].peekIntoStack().getCenterX();
+        y = board[2][13].peekIntoStack().getCenterY();
+        board[2][13].pushIntoStack(new ViewHexCentralJava(x, y));
 
-        x = board[2][14].peek().getCenterX();
-        y = board[2][14].peek().getCenterY();
-        board[2][14].push(new ViewHexCentralJava(x, y));
+        x = board[2][14].peekIntoStack().getCenterX();
+        y = board[2][14].peekIntoStack().getCenterY();
+        board[2][14].pushIntoStack(new ViewHexCentralJava(x, y));
 
         //Set mainland tiles
         for (int i = 3; i < 4; i++) {
             for (int j = 3; j < 17; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 4; i < 5; i++) {
             for (int j = 1; j < 18; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 5; i < 6; i++) {
             for (int j = 2; j < 18; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 6; i < 7; i++) {
             for (int j = 1; j < 18; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 7; i < 8; i++) {
             for (int j = 2; j < 18; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 8; i < 9; i++) {
             for (int j = 3; j < 16; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 9; i < 10; i++) {
             for (int j = 3; j < 17; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 10; i < 11; i++) {
             for (int j = 3; j < 16; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         for (int i = 11; i < 12; i++) {
             for (int j = 4; j < 14; j++) {
-                int a = board[i][j].peek().getCenterX();
-                int b = board[i][j].peek().getCenterY();
-                board[i][j].push(new ViewHexLowland(a, b));
+                int a = board[i][j].peekIntoStack().getCenterX();
+                int b = board[i][j].peekIntoStack().getCenterY();
+                board[i][j].pushIntoStack(new ViewHexLowland(a, b));
             }
         }
 
         //Here you also want to place central java tiles just like up there
 
-        int c = board[12][5].peek().getCenterX();
-        int d = board[12][5].peek().getCenterY();
-        board[12][5].push(new ViewHexCentralJava(c, d));
+        int c = board[12][5].peekIntoStack().getCenterX();
+        int d = board[12][5].peekIntoStack().getCenterY();
+        board[12][5].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[12][6].peek().getCenterX();
-        d = board[12][6].peek().getCenterY();
-        board[12][6].push(new ViewHexCentralJava(c, d));
+        c = board[12][6].peekIntoStack().getCenterX();
+        d = board[12][6].peekIntoStack().getCenterY();
+        board[12][6].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[12][7].peek().getCenterX();
-        d = board[12][7].peek().getCenterY();
-        board[12][7].push(new ViewHexCentralJava(c, d));
+        c = board[12][7].peekIntoStack().getCenterX();
+        d = board[12][7].peekIntoStack().getCenterY();
+        board[12][7].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[12][9].peek().getCenterX();
-        d = board[12][9].peek().getCenterY();
-        board[12][9].push(new ViewHexCentralJava(c, d));
+        c = board[12][9].peekIntoStack().getCenterX();
+        d = board[12][9].peekIntoStack().getCenterY();
+        board[12][9].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[12][11].peek().getCenterX();
-        d = board[12][11].peek().getCenterY();
-        board[12][11].push(new ViewHexCentralJava(c, d));
+        c = board[12][11].peekIntoStack().getCenterX();
+        d = board[12][11].peekIntoStack().getCenterY();
+        board[12][11].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[12][12].peek().getCenterX();
-        d = board[12][12].peek().getCenterY();
-        board[12][12].push(new ViewHexCentralJava(c, d));
+        c = board[12][12].peekIntoStack().getCenterX();
+        d = board[12][12].peekIntoStack().getCenterY();
+        board[12][12].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[12][13].peek().getCenterX();
-        d = board[12][13].peek().getCenterY();
-        board[12][13].push(new ViewHexCentralJava(c, d));
+        c = board[12][13].peekIntoStack().getCenterX();
+        d = board[12][13].peekIntoStack().getCenterY();
+        board[12][13].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[13][5].peek().getCenterX();
-        d = board[13][5].peek().getCenterY();
-        board[13][5].push(new ViewHexCentralJava(c, d));
+        c = board[13][5].peekIntoStack().getCenterX();
+        d = board[13][5].peekIntoStack().getCenterY();
+        board[13][5].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[13][6].peek().getCenterX();
-        d = board[13][6].peek().getCenterY();
-        board[13][6].push(new ViewHexCentralJava(c, d));
+        c = board[13][6].peekIntoStack().getCenterX();
+        d = board[13][6].peekIntoStack().getCenterY();
+        board[13][6].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[13][11].peek().getCenterX();
-        d = board[13][11].peek().getCenterY();
-        board[13][11].push(new ViewHexCentralJava(c, d));
+        c = board[13][11].peekIntoStack().getCenterX();
+        d = board[13][11].peekIntoStack().getCenterY();
+        board[13][11].pushIntoStack(new ViewHexCentralJava(c, d));
 
-        c = board[13][12].peek().getCenterX();
-        d = board[13][12].peek().getCenterY();
-        board[13][12].push(new ViewHexCentralJava(c, d));
+        c = board[13][12].peekIntoStack().getCenterX();
+        d = board[13][12].peekIntoStack().getCenterY();
+        board[13][12].pushIntoStack(new ViewHexCentralJava(c, d));
     }
 
-    public Stack<ViewHex> getStackAt(int x, int y)
+    public ViewHexStack getStackAt(int x, int y)
     {
         return board[x][y];
     }
