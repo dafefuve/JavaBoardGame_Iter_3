20e1bd0b093fed0279c1afe08d65e56caf0c6cb5
Daniel Fuentes <danielffuentes@gmail.com>
2014-04-21 22:17:25 -0400

    Improving naming conventions

--- Iter3/src/controller/CommandController.java
+++ Iter3/src/controller/CommandController.java
@@ -29,9 +29,9 @@ public class CommandController {
         return null;
     }
 
-    public PlaceVillageTileCommand placeVillageTileCommand(Facade facade){
+    public PlaceSingleVillageTileCommand placeVillageTileCommand(Facade facade){
         //TODO this needs to be done, constructor needs the proper call
-        PlaceVillageTileCommand command = new PlaceVillageTileCommand(facade);
+        PlaceSingleVillageTileCommand command = new PlaceSingleVillageTileCommand(facade);
         return command;
         //return null;
     }
