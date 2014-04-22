6f5a1a7682cf7f03e376aba2a85411102f852f07
AlexBujduveanu <alexbujduveanu@gmail.com>
2014-04-22 07:34:53 -0400

    merged and compile did not run but should be good

--- Iter3/src/controller/commands/PlaceSingleVillageTileCommand.java
+++ Iter3/src/controller/commands/PlaceSingleVillageTileCommand.java
@@ -37,11 +37,8 @@ public class PlaceSingleVillageTileCommand extends MovableCommands {
         int currentActionPoints = playerController.getItemCount("actionPoints");
         if(currentActionPoints > 0) {
 
-<<<<<<< HEAD
-            if (remainingVillageCount > 0 && !topTileComponentOfSpace.getLandType().equals("highland") && !topTileComponentOfSpace.getLandType().equals("lowland") && !topTileComponentOfSpace.getLandType().equals("irrigation") && space.getDeveloper() == null && space.getPalace() == null) {
-=======
+
             if (remainingVillageCount > 0 && boardController.checkDifferentOneSpaceBeneath(space) && !topTileComponentOfSpace.getLandType().equals("highland") && !topTileComponentOfSpace.getLandType().equals("lowland") && !topTileComponentOfSpace.getLandType().equals("irrigation") && space.getDeveloper() == null && space.getPalace() == null) {
->>>>>>> FETCH_HEAD
 
                 playerController.setItemCount("villageTile", remainingVillageCount - 1);
                 space.addTileComponent(villageToBePlaced);
